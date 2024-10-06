8e113e8244c84022aac3cf703c132bac0795b48e
AnandhaKumar S <anandhakumarnathan@gmail.com>
2024-10-06 15:39:15 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -3,7 +3,6 @@ import dotenv from "dotenv";
 import cors from "cors";
 import connectMongoDB from "./config/dbconfig.js";
 import portfolioRoute from "./routes/portfolioRoute.js";
-import path from "path";
 
 const app = express();
 dotenv.config();
@@ -18,13 +17,6 @@ app.use(
     credentials: true,
   })
 );
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client/build")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client/build/index.html"));
-  });
-}
 
 app.use("/api/portfolio", portfolioRoute);
 
